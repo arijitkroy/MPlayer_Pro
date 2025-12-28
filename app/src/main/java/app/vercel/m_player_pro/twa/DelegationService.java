df883dd213fa0655194af5fd681d88236610b3c2
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-12-28 17:09:00 +0530

    app

---
+++ app/src/main/java/app/vercel/m_player_pro/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.m_player_pro.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
